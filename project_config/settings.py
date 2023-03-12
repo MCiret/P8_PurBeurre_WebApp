5f40c9c8f976a08627f7af87708a91e9888570cf
Marie <ciret.m@gmail.com>
2023-03-12 20:11:53 +0100

    Add ALLOWED_HOSTS as enviro variable

--- project_config/settings.py
+++ project_config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY_P8')
 # DEBUG = False if os.environ.get("DJANGO_ENV", "development") == "production" else True
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DEFAULT_LOGGING['handlers']['console']['filters'] = [] # uncomment to have 500 error in terminal
 
